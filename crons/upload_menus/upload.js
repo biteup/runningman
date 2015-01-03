fb67fe075a413ffddfedf7af793b7a3fbfdc5f9c
Kelvin Tay <kelvintaywl@gmail.com>
2015-01-03 11:57:59 +0900

    [chore] split out data into chunks for instantiation

--- crons/upload_menus/upload.js
+++ crons/upload_menus/upload.js
@@ -3,6 +3,23 @@ var Spreadsheet = require('edit-google-spreadsheet');
 var schedule = require('node-schedule');
 // -- end
 
+// classes
+var Restaurant = function(name, description, email, geolocation, tags){
+	this.name = name;
+	this.description = description || '';
+	this.geolocation = geolocation || [139.710388, 35.673343];
+	this.email = email || '';
+	this.tags = tags.split(',') || [];
+};
+
+var Menu = function(name, price, images, tags) {
+	this.name = name;
+	this.price = price;
+	this.images = images || [];
+	this.tags = tags.split(',') || [];
+}
+// -- end
+
 var rowCount = 0;
 
 var readSpreadsheet = function(){
@@ -14,6 +31,7 @@ var readSpreadsheet = function(){
 	password: '123gobenri!',
 	},
 	function sheetReady(err, spreadsheet){
+
 		if(err){
 			throw err;
 		};
@@ -25,6 +43,32 @@ var readSpreadsheet = function(){
 
 			console.log("Found rows:", rows);
 			console.log("row 1:", rows["1"]);  // example of accessing row
+
+			// parse data into objects
+			var totalRows = 3, row; // TODO: ascertain total rows (sans headers)
+			var rowIndex = 3, rowSize = (totalRows - 2) + rowIndex;
+			for(; rowIndex < rowSize; rowIndex++){
+				row = rows[rowIndex.toString()];
+				var rowArr = [false];  // setup dummy value at index 0 since keys start from "1"
+				for(var key in row){
+					rowArr.push(row[key])
+				};
+				console.log('status: ', rowArr[1]);
+				console.log('restaurant: ', rowArr.slice(2, 8));
+				console.log('menu(s): ', rowArr.slice(9));
+				/*
+				var restaurant = Restaurant.apply(rowArr.slice(1,8));
+				var numMenus = parseInt(rowArr[8], 10);
+				var menus = [], iter = 9, menuArrSize = 4;
+				for(var i=1; iter < rowArr.length && i <= numMenus; iter+=menuArrSize){
+					menus.push(Menu.apply(rowArr.slice(iter, iter+menuArrSize)));
+				};
+
+				console.log('restaurant: ', restaurant);
+				console.log('menu(s): ', menus);
+				*/
+			}
+
 		});
 	});
 }
--- web.js
+++ web.js
@@ -4,4 +4,7 @@ var crons = {
 	menus : require('./crons/upload_menus/upload'),
 };
 
+console.log('RUNNING MAN v0.0.1\n--------------------------------', 
+	'\nRunning some crons now. Go grab some coffee. :)\n');
+
 crons.menus.upload();
